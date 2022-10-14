70e24f9e620c545ac10263ae8d6d3655b5580206
KarinaK <mskarinakrivko@gmail.com>
2022-10-15 01:12:36 +0200

    new project added

---
+++ js-fetch-user-cards/scripts/scripts.js
@@ -0,0 +1,38 @@
+const cards_container = document.querySelector('.cards_container');
+
+function narisovat(users) {
+
+users.forEach(user => {
+
+const item = document.createElement('div');
+
+const title = document.createElement('p');
+const status = document.createElement('div');
+
+
+item.classList.add('item');
+
+
+
+if (user.completed) { 
+    item.classList.add('status_done') 
+}
+else 
+item.classList.add('status_notdone');
+
+
+title.innerText = 'Task: '+user.title;
+status.innerText =  (user.completed) ? 'Status: done' : 'Status: not done';
+
+item.append(title, status);
+
+cards_container.append(item);
+});
+
+
+}
+
+fetch(`https://jsonplaceholder.typicode.com/todos`)
+.then(resp => resp.json()) 
+.then(json => narisovat(json)) 
+
